3d521b09c61b9ec82960f0453ca41cf5a07e6848
sofyan-dev <mchsfyan17@gmail.com>
2020-08-12 01:06:55 +0700

    1

--- searchNama.js
+++ searchNama.js
@@ -1,11 +1,8 @@
 const searchName = (key,limit,callback) => {
-    let name = ['Abigail','Alexandra','Alison','Amanda', 'Angela','Bella','Carol','Caroline','Carolyn','Deirdre','Diana','Elizabeth','Ella','Faith','Olivia','Penelope']
-    let lowName = name.map(t => t.toLowerCase())
-    let result = lowName.filter(n => n.includes(key))
-     result.length = limit ;
-    return callback(result)
-    }
-const showName = (hasil) => {
-    return hasil
-}
-console.log(searchName('an',3,showName))
+let name = ['Abigail','Alexandra','Alison','Amanda', 'Angela','Bella','Carol','Caroline','Carolyn','Deirdre',       'Diana','Elizabeth','Ella','Faith','Olivia','Penelope']
+        let lowName = name.map(t => t.toLowerCase())
+            let result = lowName.filter(n => n.includes(key))
+                result.length = limit ;
+            return callback(result)}
+        const showName = (hasil) => {return hasil}
+console.log(searchName('an',2,showName))
