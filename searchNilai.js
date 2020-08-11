b929a0529959981129843b6cf9b9cf36496ee46d
sofyan-dev <mchsfyan17@gmail.com>
2020-08-12 00:58:18 +0700

    1

---
+++ searchNilai.js
@@ -0,0 +1,15 @@
+const searchNilai = (nilaiAwal,nilaiAkhir,dataArray) => {
+        let result
+        if(nilaiAwal > nilaiAkhir){
+            result = "nilai akhir harus lebih besar dari nilai awal"
+        }else{
+            let x = dataArray.length
+            if(x < 5 ){ result = "data Array kurang dari 5"}
+            else{ let filterNilai = dataArray.filter(i => {return i > nilaiAwal && i < nilaiAkhir})
+                if(filterNilai.length == 0){ result = `Tidak ada yang sesuai`}
+                else{ result = filterNilai}
+            }}
+        console.log(result)
+}
+
+searchNilai(21,11,[8,17,14,7,3])
